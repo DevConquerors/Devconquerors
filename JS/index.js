bba7d7e8cee2bb0e2ca61a8cb1995c5815981a73
DevConquerors <79274035+DevConquerors@users.noreply.github.com>
2021-02-20 17:22:32 +0530

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,31 @@
+// alert("hello");
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+$(document).ready(function(){
+$(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        300:{
+            items:1,
+            // nav:true
+        },
+        800:{
+            items:2,
+            autoplay: true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true
+        },
+        1200:{
+            items:3,
+            autoplay: true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true
+        }
+    }
+});
